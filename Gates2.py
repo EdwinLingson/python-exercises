855f5d12e4ac4e973268379984d6b14b2e7aa696
EdwinLingson <edwin.lingson@gmail.com>
2017-12-29 21:34:23 +0530

    Add files via upload

---
+++ Gates2.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env/python
+class Gate(object):
+    """ class representing a gate. It can be any gate. """
+
+    def __init__(self, *args):
+        """ initialise the class """
+        self.input = args
+        self.output = None
+
+    def logic(self):
+        """ the intelligence to be performed """
+        raise NotImplementedError
+
+    def output(self):
+        """ the output of the gate """
+        self.logic()
+        return self.output
+
+
+class AndGate(Gate):
+    """ class representing AND gate """
+
+    def logic(self):
+        self.output = self.input[0] and self.input[1]
+
+
+class OrGate(Gate):
+    """ class representing OR gate """
+
+    def logic(self):
+        self.output = self.input[0] or self.input[1]
+
+
+class NotGate(Gate):
+    """ class representing NOT gate """
+
+    def logic(self):
+        self.output = not self.input[0]
+        
+class NandGate(AndGate,NotGate):
+    """ class representing NOT gate """
+
+    def logic(self):
+       c = AndGate(self.input[0],self.input[1])
+       c.logic()
+       d =NotGate(c.output)
+       d.logic()
+       self.output =d.output
+       
+class NorGate(OrGate,NotGate):
+    """ class representing NOT gate """
+
+    def logic(self):
+       c = OrGate(self.input[0],self.input[1])
+       c.logic()
+       d =NotGate(c.output)
+       d.logic()
+       self.output =d.output
+      
+
+
+def Gates(**kwargs):
+	gate=kwargs['Gates']
+	#print (kwargs['Gates'])
+	#print ("oper = ",oper)
+	a=kwargs['input1']
+	b=kwargs['input2']
+	
+	if(gate=='not'):
+		x=NotGate(a,b)
+		x.logic()
+		print ("Not Operation: of ",a," and ",b," is ",x.output)
+	if(gate=='and'):
+		x=AndGate(a,b)
+		x.logic()
+		print ("And Operation: of ",a," and ",b," is ",x.output)
+	if(gate=='or'):
+		x=OrGate(a,b)
+		x.logic()
+		print ("Or Operation: of ",a," and ",b," is ",x.output)
+	if(gate=='nor'):
+		x=NorGate(a,b)
+		x.logic()
+		print ("Nor Operation: of ",a," and ",b," is ",x.output)
+	if(gate=='nand'):
+		x=NandGate(a,b)
+		x.logic()
+		print ("Nand Operation: of ",a," and ",b," is ",x.output)	
+
+
+#ase(Gates='not',input1=1,input2=0)
+#if __name__=="__main__":
+#	ase(Gates='and',input1=1,input2=0)
